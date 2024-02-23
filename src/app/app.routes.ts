2cae98dab03f79bb61bedd6414b7e1e7af397189
Janio <janiojunior@gmail.com>
2024-02-23 11:10:42 -0300

    Crud completo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { EstadoListComponent } from './components/estado/estado-list/estado-list.component';
 import { EstadoFormComponent } from './components/estado/estado-form/estado-form.component';
+import { estadoResolver } from './components/estado/resolver/estado-resolver';
 
 export const routes: Routes = [
     { path: 'estados', component: EstadoListComponent, title: 'Lista de Estados'},
     { path: 'estados/new', component: EstadoFormComponent, title: 'Novo Estado'},
+    { path: 'estados/edit/:id', component: EstadoFormComponent, resolve: {estado: estadoResolver}},
 ];
--- src/app/components/estado/estado-form/estado-form.component.ts
+++ src/app/components/estado/estado-form/estado-form.component.ts
@@ -4,13 +4,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { EstadoService } from '../../../services/estado.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { NgIf } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Estado } from '../../../models/estado.model';
 
 @Component({
   selector: 'app-estado-form',
   standalone: true,
-  imports: [NgIf, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [NgIf, ReactiveFormsModule, MatFormFieldModule,
+    MatInputModule, MatButtonModule, MatCardModule, MatToolbarModule, RouterModule],
   templateUrl: './estado-form.component.html',
   styleUrl: './estado-form.component.css'
 })
@@ -19,27 +23,58 @@ export class EstadoFormComponent {
   formGroup: FormGroup;
 
   constructor(private formBuilder: FormBuilder,
-              private estadoService: EstadoService,
-              private router: Router) {
+    private estadoService: EstadoService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute) {
+
+    const estado: Estado = activatedRoute.snapshot.data['estado'];
 
     this.formGroup = formBuilder.group({
-      nome: ['', Validators.required],
-      sigla: ['', Validators.required]
+      id: [(estado && estado.id) ? estado.id : null],
+      nome: [(estado && estado.nome) ? estado.nome : '', Validators.required],
+      sigla: [(estado && estado.sigla) ? estado.sigla : '', Validators.required]
     });
 
   }
 
-  onSubmit() {
+  salvar() {
     if (this.formGroup.valid) {
-      const novoEstado = this.formGroup.value;
-      this.estadoService.salvar(novoEstado).subscribe({
-        next: (estadoCadastrado) => {
-          this.router.navigateByUrl('/estados');
-        },
-        error: (err) => {
-          console.log('Erro ao salvar' + JSON.stringify(err));
-        }
-      });
+      const estado = this.formGroup.value;
+      if (estado.id ==null) {
+        this.estadoService.insert(estado).subscribe({
+          next: (estadoCadastrado) => {
+            this.router.navigateByUrl('/estados');
+          },
+          error: (err) => {
+            console.log('Erro ao Incluir' + JSON.stringify(err));
+          }
+        });
+      } else {
+        this.estadoService.update(estado).subscribe({
+          next: (estadoAlterado) => {
+            this.router.navigateByUrl('/estados');
+          },
+          error: (err) => {
+            console.log('Erro ao Editar' + JSON.stringify(err));
+          }
+        });
+      }
+    }
+  }
+
+  excluir() {
+    if (this.formGroup.valid) {
+      const estado = this.formGroup.value;
+      if (estado.id != null) {
+        this.estadoService.delete(estado).subscribe({
+          next: () => {
+            this.router.navigateByUrl('/estados');
+          },
+          error: (err) => {
+            console.log('Erro ao Excluir' + JSON.stringify(err));
+          }
+        });
+      }
     }
   }
 
