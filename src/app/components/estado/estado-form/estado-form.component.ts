6797d00de5718619e0c7a11aa21f34bed48150f4
Janio <janiojunior@gmail.com>
2024-03-22 11:30:18 -0300

    paginacao

--- src/app/components/estado/estado-form/estado-form.component.ts
+++ src/app/components/estado/estado-form/estado-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -9,6 +9,8 @@ import { NgIf } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { Estado } from '../../../models/estado.model';
+import { EmptyError, Observable } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-estado-form',
@@ -31,34 +33,57 @@ export class EstadoFormComponent {
 
     this.formGroup = formBuilder.group({
       id: [(estado && estado.id) ? estado.id : null],
-      nome: [(estado && estado.nome) ? estado.nome : '', Validators.required],
-      sigla: [(estado && estado.sigla) ? estado.sigla : '', Validators.required]
+      nome: [(estado && estado.nome) ? estado.nome : '', 
+            Validators.compose([Validators.required, 
+                                Validators.minLength(4)])],
+      sigla: [(estado && estado.sigla) ? estado.sigla : '', 
+            Validators.compose([Validators.required,
+                                Validators.minLength(2)])]
     });
 
   }
 
   salvar() {
+    // marca todos os campos do formulario como 'touched'
+    this.formGroup.markAllAsTouched();
     if (this.formGroup.valid) {
       const estado = this.formGroup.value;
-      if (estado.id ==null) {
-        this.estadoService.insert(estado).subscribe({
-          next: (estadoCadastrado) => {
-            this.router.navigateByUrl('/estados');
-          },
-          error: (err) => {
-            console.log('Erro ao Incluir' + JSON.stringify(err));
+
+      // operacao obtem o retorno de um observable de insert ou update
+      const operacao = estado.id == null
+      ? this.estadoService.insert(estado)
+      : this.estadoService.update(estado);
+
+      // realiza a operacao e trata a resposta.
+      operacao.subscribe({
+        next: () => this.router.navigateByUrl('/estados'),
+        error: (error: HttpErrorResponse) => {
+          console.log('Erro ao salvar' + JSON.stringify(error));
+          this.tratarErros(error);
+        }
+      });
+    }
+  }
+
+  tratarErros(error: HttpErrorResponse) {
+    if (error.status === 400) {
+      // erros relacionados a campos
+      if (error.error?.errors) {
+        error.error.errors.forEach((validationError: any) => {
+          // obs: o fieldName tem o mesmo valor da api
+          const formControl = this.formGroup.get(validationError.fieldName);
+          console.log(validationError);
+          if (formControl) {
+            console.log(formControl);
+            formControl.setErrors({ apiError: validationError.message });
           }
         });
-      } else {
-        this.estadoService.update(estado).subscribe({
-          next: (estadoAlterado) => {
-            this.router.navigateByUrl('/estados');
-          },
-          error: (err) => {
-            console.log('Erro ao Editar' + JSON.stringify(err));
-          }
-        });
-      }
+      };
+    } else if (error.status < 400) {
+        // Erro genérico não relacionado a um campo específico.
+        alert(error.error?.message || 'Erro genérico no envio do formulário.');
+    } else if (error.status >= 500) {
+        alert('Erro interno do servidor. Por favor, tente novamente mais tarde.');
     }
   }
 
@@ -76,6 +101,34 @@ export class EstadoFormComponent {
         });
       }
     }
+  }
+
+  errorMessages: {[controlName: string]: {[errorName: string] : string}} = {
+    nome: {
+      required: 'O nome deve ser informado.',
+      minlength: 'O nome deve possuir ao menos 4 caracteres.'
+    },
+    sigla: {
+      required: 'A sigla deve ser informada.',
+      minlength: 'A sigla deve possuir 2 caracteres.',
+      maxlength: 'A sigla deve possuir 2 caracteres.',
+      apiError: ' ' // mensagem da api
+    }
+  }
+
+  getErrorMessage(controlName: string, errors: ValidationErrors | null | undefined): string {
+    if (!errors) {
+      return '';
+    }
+    // retorna a mensagem de erro
+    for (const errorName in errors) {
+      if (errors.hasOwnProperty(errorName) && 
+          this.errorMessages[controlName][errorName]) {
+            return this.errorMessages[controlName][errorName];
+      }
+    }
+
+    return 'Erro não mapeado (entre em contato com o desenvolvedor)';
   }
 
 }
--- src/app/components/estado/estado-list/estado-list.component.ts
+++ src/app/components/estado/estado-list/estado-list.component.ts
@@ -7,12 +7,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-estado-list',
   standalone: true,
   imports: [NgFor, MatTableModule, MatToolbarModule, MatIconModule
-  , MatButtonModule, RouterModule],
+    , MatButtonModule, RouterModule, MatPaginatorModule],
   templateUrl: './estado-list.component.html',
   styleUrl: './estado-list.component.css'
 })
@@ -20,14 +21,31 @@ export class EstadoListComponent implements OnInit {
   displayedColumns: string[] = ['id', 'nome', 'sigla', 'acao'];
   estados: Estado[] = [];
 
+  // variaveis de controle de paginacao
+  totalRecords = 0;
+  pageSize = 2;
+  page = 0;
+
   constructor(private estadoService: EstadoService) {
 
   }
 
   ngOnInit(): void {
-    this.estadoService.findAll().subscribe(data => {
+    this.estadoService.findAll(this.page, this.pageSize).subscribe(data => {
       this.estados = data;
-    })
+      console.log(this.estados);
+    });
+
+    this.estadoService.count().subscribe(data => {
+      this.totalRecords = data;
+      console.log(this.totalRecords);
+    });
+  }
+  // Método para paginar os resultados
+  paginar(event: PageEvent): void {
+    this.page = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.ngOnInit();
   }
 
 }
